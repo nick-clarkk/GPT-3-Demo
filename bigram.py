83c639dfbfe04d91139e4aaa3ad8fbed21d607ea
Nicholas Clark <nicholasliemclark@gmail.com>
2026-09-04 14:50:01 -0700

    fixed bigram bug, final update

--- bigram.py
+++ bigram.py
@@ -2,25 +2,26 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-#hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-context_length = 8 # what is the maximum context length for predictions?
+# hyperparameters
+batch_size = 32 # num of independent sequences we process in parallel
+context_length = 8 # max context length for predictions
 max_iters = 3000
 eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32 
+# ------------
 
 torch.manual_seed(1337)
 
-# using https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
+# here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
-# mapping from characters to integers
+# create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
@@ -28,7 +29,7 @@ decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integ
 
 # Train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
-n = int(0.9*len(data))
+n = int(0.9*len(data)) #train-test split
 train_data = data[:n]
 val_data = data[n:]
 
@@ -42,10 +43,10 @@ def get_batch(split):
     x, y = x.to(device), y.to(device)
     return x, y
 
-@torch.no_grad() #telling pyTorch not to compute gradients for this function, saves memory and computations
+@torch.no_grad()
 def estimate_loss():
     out = {}
-    model.eval() #for inference mode (disables dropout)
+    model.eval() # for evaluation, we don't want to update any parameters
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
@@ -53,28 +54,22 @@ def estimate_loss():
             logits, loss = model(X, Y)
             losses[k] = loss.item()
         out[split] = losses.mean()
-    model.train() #for training mode (enables dropout, normalization, etc.)
+    model.train() # back to training mode
     return out
 
-# super simple bigram model
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self):
+    def __init__(self, vocab_size):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embd) #n_embd is the size of the embedding vector for each token
-        self.position_embedding_table = nn.Embedding(context_length, n_embd) #encode position of tokens relative to others in context window
-        self.lm_head = nn.Linear(n_embd, vocab_size) #linear layer that maps from embedding space to vocab space
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
     def forward(self, idx, targets=None):
-        B, T = idx.shape
 
         # idx and targets are both (B,T) tensor of integers
-        tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
-        x = tok_emb + pos_emb # token identiy + position information
-        logits = self.lm_head(x) # (B,T,vocab_size)
+        logits = self.token_embedding_table(idx) # (B,T,C)
 
+        # if no targets are provided, we are in inference mode, so just return the logits
         if targets is None:
             loss = None
         else:
@@ -88,33 +83,37 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
-            # get the predictions
+            # predictions
             logits, loss = self(idx)
-            logits = logits[:, -1, :] # focus on last step, becomes (B, C)
+            # pick out the logits at the final step and apply softmax
+            logits = logits[:, -1, :] # becomes (B, C)
             probs = F.softmax(logits, dim=-1) # (B, C)
-            idx_next = torch.multinomial(probs, num_samples=1) # predict next token in each batch (B, 1)
-            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1) # append pred to running sequence
+            # sample from the distribution and add it to the running sequence
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel()
+model = BigramLanguageModel(vocab_size)
 m = model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 for iter in range(max_iters):
 
-    # evlauate loss every other step
+    # every other eval_interval evaluate the loss on train and val sets
     if iter % eval_interval == 0:
         losses = estimate_loss()
         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
+    # sample a batch of data
     xb, yb = get_batch('train')
 
+    # optimize the model
     logits, loss = model(xb, yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
 
-# generation
+# generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
